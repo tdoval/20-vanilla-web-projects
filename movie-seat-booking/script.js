211424156bc086ec2e6ec54e30972a40bfb64a7f
Thiago Araujo <thiagodovalaraujo@gmail.com>
2021-08-30 10:49:50 -0300

    movie seat booking - DOM and LocalStorage

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -4,22 +4,55 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie')
 
+populateUI();
+
 let ticketPrice = parseInt(movieSelect.value);
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 // Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+    // Copy selected seats into arr
+    // Map trough array
+    // return a new array of indexes
+    const seatsIndex = [...selectedSeats].map((seat) =>  [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from localstorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
@@ -31,3 +64,6 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
     }
 });
+
+// Initial count and total set
+updateSelectedCount();
