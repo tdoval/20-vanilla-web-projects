34b64f38fa42afaed44ecdb7d31cd68a08310dab
tdoval <thiagodovalaraujo@gmail.com>
2021-09-29 18:14:31 -0300

    menu-slider-modal landing page

---
+++ slider-menu-modal/script.js
@@ -0,0 +1,22 @@
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+// Toggle nav
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+
+// Show modal
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+// Hide modal
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+// Hide modal on outside click
+window.addEventListener("click", (e) => {
+  console.log(e.target);
+  console.log(modal);
+  e.target == modal ? modal.classList.remove("show-modal") : false;
+});
